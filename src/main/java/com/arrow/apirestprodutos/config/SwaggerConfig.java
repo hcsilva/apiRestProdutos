989b7e30c6b0f9554b96e75b92321ed260d8e9a2
hcsilva <henriquec.dasilva@hotmail.com>
2020-06-06 14:49:33 -0300

    Ajuste no config do swagger

--- src/main/java/com/arrow/apirestprodutos/config/SwaggerConfig.java
+++ src/main/java/com/arrow/apirestprodutos/config/SwaggerConfig.java
@@ -31,7 +31,7 @@ public class SwaggerConfig {
 
 		ApiInfo apiInfo = new ApiInfo("Produtos API REST", "API REST de cadastro de produtos.", "1.0",
 				"Terms of Service",
-				new Contact("Michelli Brito", "https://www.youtube.com/michellibrito", "michellidibrito@gmail.com"),
+				new Contact("Henrique", " ", " "),
 				"Apache License Version 2.0", "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>());
 
 		return apiInfo;
